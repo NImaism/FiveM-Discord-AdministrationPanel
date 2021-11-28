cef190f791fa1857e19583c9059dd445ada972e1
NIma <80697993+NImaism@users.noreply.github.com>
2021-11-28 10:18:53 +0330

    Enjoy !

--- IsM Panel/Main.lua
+++ IsM Panel/Main.lua
@@ -1,21 +1,17 @@
-datas = {}
+local the_data_need
 CreateThread(function()
     while true do
-        Wait(1000*2) 
-        datas['File'] = LoadResourceFile(GetCurrentResourceName(), "./request.json") 
-        if datas['File'] and json.decode(datas['File']) and json.decode(datas['File']) ~= datas['File'] then
-            datas['File'] = json.decode(datas['File'])
-        else
-            datas['File'] = nil
+        Wait(1000*3)
+        the_data_need = LoadResourceFile(GetCurrentResourceName(), "./request.json") 
+        if the_data_need then
+            the_data_need = json.decode(the_data_need)
         end
-        if not datas['File'] then
-            SaveResourceFile(GetCurrentResourceName(), 'request.json', json.encode({}))
-            datas['File'] = {}
-       end
-       for _,i in pairs(datas['File']) do
-          ExecuteCommand(i)
-       end  
-       SaveResourceFile(GetCurrentResourceName(), 'request.json', '{}')
-
+        if type(the_data_need) == 'table' then
+            SaveResourceFile(GetCurrentResourceName(), 'request.json', '')
+            for _,i in pairs(the_data_need) do
+                ExecuteCommand(i)
+            end
+        end
     end
 end)
+
